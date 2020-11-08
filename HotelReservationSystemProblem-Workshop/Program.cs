413c4f91b1806e0fc85e5267cd21a3885567b4c2
dheer meena <dheer1998meena@gmail.com>
2020-11-08 23:24:32 +0530

    [Dheer] Add: Implemented the functionality to find the best rated hotel and its cost for date range

--- HotelReservationSystemProblem-Workshop/Program.cs
+++ HotelReservationSystemProblem-Workshop/Program.cs
@@ -85,5 +85,28 @@ namespace HotelReservationSystemProblem_Workshop
                 FindCheapest(hotelReservation);
             }
         }
+        public static void FindBest(HotelReservation hotelReservation)
+        {
+            Console.WriteLine("Cheapest Best Rated Hotel");
+            Console.Write("Enter the date range : ");
+            var input = Console.ReadLine();
+            string[] dates = input.Split(',');
+            try
+            {
+                var startDate = Convert.ToDateTime(dates[0]);
+                var endDate = Convert.ToDateTime(dates[1]);
+                var cheapestHotel = hotelReservation.FindCheapestBestRatedHotel(startDate, endDate);
+                foreach (Hotel h in cheapestHotel)
+                {
+                    var cost = hotelReservation.CalculateCost(h, startDate, endDate);
+                    Console.WriteLine("Hotel : {0}, Rating: {1}, Total Cost : {2}", h.hotelName, h.rating, cost);
+                }
+            }
+            catch
+            {
+                Console.Write("Enter the correct date range \n");
+                FindCheapest(hotelReservation);
+            }
+        }
     }
 }
